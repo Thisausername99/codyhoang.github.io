ac4a71d4b99fe4c0ccc2a0b9cf65644204834d0c
Thisausername99 <cody_hoang99@yahoo.com>
2020-07-20 16:24:18 +0700

    prevent icon jump with font awesome plugin

--- personal-site/src/components/Marquee/Banner.js
+++ personal-site/src/components/Marquee/Banner.js
@@ -1,5 +1,6 @@
 import React from "react"
 import styles from "./_marque.module.scss"
+import '@fortawesome/free-brands-svg-icons';
 
 class Banner extends React.Component{
     componentDidMount() {
