cd750f746a4acc0c398b28e6b38ae7834ca85019
Thisausername99 <cody_hoang99@yahoo.com>
2021-01-14 16:23:49 +0700

    add audio for name

--- personal-site/src/components/Header.js
+++ personal-site/src/components/Header.js
@@ -1,18 +1,45 @@
 import PropTypes from 'prop-types'
 import React from 'react'
 import RotatingText from 'react-rotating-text'
+import Drill from './../audio/core_drill_sound.mp3'
+import { useState, useEffect } from "react";
+
 // import 'ReactRotatingText.css';
 
 // import Typewriter from 'typewriter-effect';
 
-// const Type =
-// <Typewriter
-//   options={{
-//     strings: ['Hello', 'World'],
-//     autoStart: true,
-//     loop: true,
-//   }}
-// />
+const useAudio = () => {
+  const [audio] = useState(new Audio(Drill));
+  const [playing, setPlaying] = useState(false);
+
+  const toggle = () => setPlaying(!playing);
+
+  useEffect(() => {
+      playing ? audio.play() : audio.pause();
+    },
+    [playing]
+  );
+
+  useEffect(() => {
+    audio.addEventListener('ended', () => setPlaying(false));
+    return () => {
+      audio.removeEventListener('ended', () => setPlaying(false));
+    };
+  }, []);
+
+  return [playing, toggle];
+};
+
+const Player = ({ url }) => {
+  const [playing, toggle] = useAudio(url);
+
+  return (
+    <div>
+      <h1 className="fancy" onMouseEnter={toggle}> Huy Hoang </h1>
+    </div>
+  );
+};
+
 
 const Header = props => (
     <header id="header" style={props.timeout ? { display: 'none' } : {}}>
@@ -21,7 +48,12 @@ const Header = props => (
       </div>
       <div className="content">
         <div className="inner">
-          <h1 className="fancy">Huy Hoang</h1>
+
+           {/* <h1 className="fancy" onMouseOver = {drillPlay}>
+
+          Huy Hoang</h1> */}
+          <Player></Player>
+
           <p className="typewritter">
           <RotatingText items={['Programmer', 'Software Engineer', 'Gamer']} />
           </p>
